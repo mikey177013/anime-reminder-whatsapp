c8339ee3887d33dee5ecafbf129471f5a8324fe7
𝚸𝚮𝚯𝚵𝚴𝚰𝚾 <piyushguptaofficial07042009@gmail.com>
2025-10-06 13:25:17 +0530

    Update unblock.ts

--- src/commands/unblock.ts
+++ src/commands/unblock.ts
@@ -17,40 +17,51 @@ export default class extends BaseCommand {
     ): Promise<void> => {
         let users: string[] = []
         let isSkipped = false
+
         if (M.mentioned.length) users.push(...M.mentioned)
         if (M.quoted && !M.quoted.key.fromMe) users.push(M.quoted.sender.id)
-        if (flags.id)
+        if (flags.id) {
             users.push(
                 ...flags.id
                     .split(',')
                     .map((x) => x.split('@')[0].concat('@s.whatsapp.net'))
             )
+        }
+
         const blocklist = await this.client.sock.fetchBlocklist()
         users = users.filter(
             (user) =>
                 user !== this.client.cleanId(this.client.sock.user?.id || '')
         )
-        if (!users.length)
+
+        if (!users.length) {
             return void (await M.reply(
-                "Provide the users to unblock by mentioning, replying or including --id=91xxxx (91 is the country code & xxxx is the user's whatsapp id) after the command text (or by doing all of it)."
+                "Provide users to unblock by mentioning, replying, or using --id=91xxxx (91 is the country code & xxxx is the user's WhatsApp id).\n\n*[You can't unblock the bot itself]*"
             ))
+        }
+
         let text = '🟩 *Unblocked:*'
         for (const id of users) {
             if (!blocklist.includes(id)) {
                 isSkipped = true
                 continue
             }
-            await this.client.sock.updateBlockStatus(id, 'block')
-            text += `\n*@${id.split('@')[0]}*`
+            try {
+                await this.client.sock.updateBlockStatus(id, 'unblock')
+                text += `\n*@${id.split('@')[0]}*`
+            } catch {
+                isSkipped = true
+            }
         }
-        return void (await M.reply(
+
+        await M.reply(
             text.concat(
                 isSkipped
-                    ? '\n*[Some users were ignored as they were not blocked by the bot]*'
+                    ? '\n\n🟨 *Note:* Some users were skipped (either not blocked or failed to unblock).'
                     : ''
             ),
             'text',
             users
-        ))
+        )
     }
 }
